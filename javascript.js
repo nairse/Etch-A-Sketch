0b3a312462639b6bfb67b24b9756c9e1dbc68414
nairse <nairse@mail.uc.edu>
2022-05-11 15:05:16 -0400

    added a color change effect on hover on the grid squares

--- javascript.js
+++ javascript.js
@@ -4,4 +4,10 @@ for(let i = 0; i < 256; i++){
     container.appendChild(document.createElement('div'));
    }
 
+const div = document.querySelectorAll("div > div");
+console.log(div); 
+div.forEach(element => {element.addEventListener("mouseover", () => element.setAttribute("style", "background-color:red;"));
+    
+});
+
 
